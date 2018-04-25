d9b357ef3c8b64b47685db5c2b2db3112708c4ef
yuweijun <yuweijun@live.com>
2018-04-25 20:20:14 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# 书法碑帖
+
+书法碑帖主要来源于新浪微盘，百度云等网络共享，部分来自新浪博客，网易博客及书法论坛，碑帖说明主要以百度百科为主。
+
+书法碑帖图片都有三种尺寸，一种是碑帖目录下的原尺寸的图片，个别书法长卷有近`40M`一张图。另二种是在`w100`和`w1000`目录下的宽度分别为`100像素`和`1000像素`的缩略图，用以在网页上显示查看。如果原图宽度就是小于`1000像素`的，则`w1000`目录下的图片实际上就是原图。
+
+本项目基于`nodejs/expressjs`开发，源码全部开源托管在[github](https://github.com/yuweijun/shufabeitie)上，碑帖介绍说明编辑页面的默认用户名密码可以通过`config/users.json`配置，密码加密算法为`sha1`，`session-cookie`可以通过`config/session-config.json`配置。
+
+## 安装：
+
+1. 首先图片处理依赖于`imagemagick`库，先安装好`imagemagick`。
+2. 然后通过以下命令安装`nvm`和`node`，并运行项目。
+
+```
+$ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+$ nvm install 0.12.2
+$ npm install pm2 -g
+$ npm install
+$ pm2 start processes.json
+```
+
+## create index.html for beitie directory
+
+```
+bin/index_html public/beitie
+```
---
+++ app.js
@@ -0,0 +1,80 @@
+var fs = require('fs');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('cookie-session');
+
+var routes = require('./routes/index');
+var shufa = require('./routes/shufa');
+var search = require('./routes/search');
+
+var sessionConfig = {
+    "name": "sessionid",
+    "secret": "shufabeitie.com",
+    "maxAge": 21600000
+};
+var sessionConfigFile = './config/session-config';
+if (fs.existsSync(sessionConfigFile)) {
+    sessionConfig = require(sessionConfigFile);
+}
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(session(sessionConfig));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/shufa', shufa);
+app.use('/search', search);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    // pretty print html
+    app.locals.pretty = true;
+
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
